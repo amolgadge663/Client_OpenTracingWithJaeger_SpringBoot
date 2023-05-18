688a897356577204a60051c8dd45aaf621434cc7
Amol Gadage <AGadage@a10networks.com>
2023-05-18 22:23:21 +0530

    initial commit

---
+++ src/main/java/com/amolsoftwares/www/ClientJaeger/ClientJaegerApplication.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.www.ClientJaeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientJaegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientJaegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amolsoftwares/www/ClientJaeger/ClientJaegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.www.ClientJaeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientJaegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
